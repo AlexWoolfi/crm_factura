38b3a828f2dfe86a8a38d26b2deb0ab465243620
AleksOdessa <aleksjavadev@gmail.com>
2021-12-11 10:01:24 +0200

    11.12.2021

--- src/main/java/ru/facturasmm/crm_factura/entity/users/employees/Employee.java
+++ src/main/java/ru/facturasmm/crm_factura/entity/users/employees/Employee.java
@@ -4,10 +4,7 @@ package ru.facturasmm.crm_factura.entity.users.employees;
 import lombok.*;
 import ru.facturasmm.crm_factura.entity.groupsvk.GroupOFCompany;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -17,37 +14,52 @@ import java.util.List;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Table(name="employees")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
 
+  @Column(name="name")
    private String name;
 
+   @Column(name="surname")
    private String surname;
 
+   @Column(name="department")
    private String department;
 
+   @Column(name="phoneNumber")
    private String phoneNumber;
 
+   @Column(name="email")
    private String email;
 
+   @Column(name="country")
    private String country;
 
+   @Column(name="city")
    private  String city;
 
+   @Column(name="urlVK")
    private String urlVK;
 
+   @Column(name="salary")
    private int salary;
 
+   @Column(name="percentBonus")
    private int percentBonus;
 
+   @Column(name="wallet")
    private String wallet;
 
+   @Column(name="urlimage")
    private String urlimage;
 
-   private boolean admin;
+   @Column(name="adminT")
+   private boolean adminT;
 
 
 }
